c4fead1205e2b7a5feaf31dbb44359ee357f643f
Ashley G <ashleyg3@uw.edu>
2019-01-27 23:33:50 -0800

    Scene changing test
    Functionality for 3 "worlds" and a menu

---
+++ Assets/Plugins/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void Start() {
+
+    }
+    
+    public void NextScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
